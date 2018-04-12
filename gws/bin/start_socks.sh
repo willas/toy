6744d1810e3b48c9b492e7f323cc56a4a95edc8e
yinhongbo <root@yinhongbo.com>
2018-04-12 14:56:10 +0800

    sock挂自动重启

--- gws/bin/start_socks.sh
+++ gws/bin/start_socks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 source /etc/profile
 
-exec ssh chenfei@47.90.42.37 -N -D $1 >> /dev/null 2>&1
+while true; do
+    ssh chenfei@47.90.42.37 -N -D $1 >> /dev/null 2>&1
+    sleep 1
+done
--- gws/start.sh
+++ gws/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ps aux|grep supervisord|grep -v grep |awk '{print $2}' | xargs kill -9
+ps aux|grep start_socks.sh|grep -v grep |awk '{print $2}' | xargs kill -9
 ps aux|grep 126|grep -v grep |awk '{print $2}' | xargs kill -9
 pkill nginx
 
