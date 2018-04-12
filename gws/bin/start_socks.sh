0b57e9a9f1bb5832a4fe2dc8fbbbc52083ede27c
yinhongbo <root@yinhongbo.com>
2018-04-12 14:43:53 +0800

    统一配置，忽略提交

--- gws/bin/start_socks.sh
+++ gws/bin/start_socks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /etc/profile
 
-exec ssh root@47.90.42.37 -N -D $1 >> /dev/null 2>&1
+exec ssh chenfei@47.90.42.37 -N -D $1 >> /dev/null 2>&1
--- gws/start_office.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ps aux|grep supervisord|grep -v grep |awk '{print $2}' | xargs kill -9
-ps aux|grep 126|grep -v grep |awk '{print $2}' | xargs kill -9
-pkill nginx
-
-/usr/bin/supervisord -c /home/sre/toy/gws/app/supervisor.conf
