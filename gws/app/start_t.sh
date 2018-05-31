b1ba827bfb295c656d0bec7a22000452c02c5bc3
chenfei60 <chenfei60@wanda.cn>
2018-05-31 17:41:57 +0800

    跳转运行时配置路径

--- gws/app/start_t.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ps aux|grep supervisord|grep -v grep |awk '{print $2}' | xargs kill -9
-ps aux|grep 126|grep -v grep |awk '{print $2}' | xargs kill -9
-pkill nginx
-
-/usr/bin/supervisord -c START_SUPERVISOR_CONF
